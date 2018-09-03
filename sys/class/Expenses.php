8fbc7efad5c91cc1570ac09fe7196b42d52175d2
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-09-03 13:17:05 +0200

    get the last 5 expenses and sum all expenses

---
+++ sys/class/Expenses.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+require_once 'Database.php';
+
+class Expenses extends Database{
+    //argument: type: INT, the limit of results
+
+    public function lastExpenses($limit){
+        $lastExpensesSql = "SELECT *, category.id as id_from_category FROM `expenses`
+                INNER JOIN category ON expenses.category_id = category.id
+                ORDER BY `expense_date` LIMIT $limit";
+
+        $lastExpenses = $this->query($lastExpensesSql);
+        return $lastExpenses;
+    }
+
+    public function allExpensesMonth(){
+        $monthStart =  date('Y-m-').'01 00:00:00';
+        $monthEnd  = date('Y-m-t'). ' 23:59:59';
+        $allExpensesMonthSql = "SELECT `expense_price`
+                                FROM `expenses`
+                                WHERE `expense_date` BETWEEN '$monthStart' AND '$monthEnd';";
+        $allExpensesMonth = $this->query($allExpensesMonthSql);
+        $sum  = 0;
+        foreach ($allExpensesMonth as $price){
+            $sum+=$price['expense_price'];
+        }
+        return $sum;
+    }
+
+}
+//just if you want check something
+//$x = new Expenses();
+//var_dump($x->allExpensesMonth());
