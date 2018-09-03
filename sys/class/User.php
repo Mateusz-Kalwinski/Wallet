6fca8231d0c287b396337642cd0444b304664009
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-09-03 13:17:56 +0200

    get user budget

---
+++ sys/class/User.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+require_once 'Database.php';
+
+class User extends Database{
+
+    public function userData($id){
+        $userDataSql = "SELECT `name`, `email`, `budget`
+                        FROM `user`
+                        WHERE `id` = $id";
+
+        $userData = $this->query($userDataSql);
+        return $userData;
+
+    }
+
+}
