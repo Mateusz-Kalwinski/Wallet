78424a3698072404d788254c46d987bae94e591c
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-09-04 13:24:59 +0200

    get a budget start from database

--- sys/class/User.php
+++ sys/class/User.php
@@ -6,7 +6,7 @@ require_once 'Database.php';
 class User extends Database{
 
     public function userData($id){
-        $userDataSql = "SELECT `name`, `email`, `budget`
+        $userDataSql = "SELECT `name`, `email`, `budget`, `budget_start`
                         FROM `user`
                         WHERE `id` = $id";
 
