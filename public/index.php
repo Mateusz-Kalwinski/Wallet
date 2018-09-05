315162a5749bbd695dc8802fc0988cd45df463df
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-09-05 11:40:46 +0200

    comparison of monthly expenses chart

--- public/index.php
+++ public/index.php
@@ -48,7 +48,13 @@ $user = new User();
         </div>
         <?php
             $userData = $user->userData(1);
-            $sumExpenses = $expenses->allExpensesMonth();
+            $sumExpenses_0 = $expenses->allExpensesMonth(0);
+            $sumExpenses_1 = $expenses->allExpensesMonth(1);
+            $sumExpenses_2 = $expenses->allExpensesMonth(2);
+            $sumExpenses_3 = $expenses->allExpensesMonth(3);
+            $sumExpenses_4 = $expenses->allExpensesMonth(4);
+
+            $dataLabel = date('n');
 
         ?>
         <div class="col-lg-4 col-md-4 col-sm-12">
@@ -61,7 +67,9 @@ $user = new User();
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <h1 class="text-purple text-center">W tym miesiącu wydałeś:</h1>
-            <h4 class="text-grey text-center"><?=$sumExpenses?> zł</h4>
+            <h4 class="text-grey text-center"><?=$sumExpenses_0?> zł</h4>
+            <br>
+            <canvas id="myChart1" style="width: 200px; height: 200px;" width="1300" height="340"></canvas>
         </div>
     </div>
 </div>
@@ -90,6 +98,58 @@ $user = new User();
                 backgroundColor: [
                     gradientStroke1,
                     gradientStroke,
+                ],
+                borderColor: [
+                    'rgba(132, 69, 255, 1)',
+                    'rgba(54, 159, 255, 1)',
+                ],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            }
+        }
+    });
+
+    var ctx = document.getElementById("myChart1").getContext('2d');
+
+
+    var gradientStroke = ctx.createLinearGradient(500, 430, 0, 0);
+    gradientStroke.addColorStop(0.4, '#8445ff');
+    gradientStroke.addColorStop(0.7, '#369fff');
+
+    var gradientStroke1 = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke1.addColorStop(0.35, '#00cef2');
+    gradientStroke1.addColorStop(0.6, '#54fbb5');
+
+    var gradientStroke2 = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke2.addColorStop(0.35, '#f9e86f');
+    gradientStroke2.addColorStop(0.6, '#f1688d');
+
+    var gradientStroke3 = ctx.createLinearGradient(500, 430, 0, 0);
+    gradientStroke3.addColorStop(0.55, '#27f9c6');
+    gradientStroke3.addColorStop(0.7, '#44acf1');
+
+    var gradientStroke4 = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke4.addColorStop(0.55, '#f97ca2');
+    gradientStroke4.addColorStop(0.7, '#c185f1');
+
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["<?=$dataLabel?>", "<?=$dataLabel -1?>", "<?=$dataLabel -2?>", "<?=$dataLabel -3?>", "<?=$dataLabel -4?>"],
+            datasets: [{
+                label: '# of Votes',
+                data: [<?=$sumExpenses_0?>, <?=$sumExpenses_1?>, <?=$sumExpenses_2?>, <?=$sumExpenses_3?>, <?=$sumExpenses_4?>],
+                backgroundColor: [
+                    gradientStroke1,
+                    gradientStroke,
+                    gradientStroke2,
+                    gradientStroke3,
+                    gradientStroke4,
+
                 ],
                 borderColor: [
                     'rgba(132, 69, 255, 1)',
