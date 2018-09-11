ac9814482d9088ee8368d1788f8f9951ecf83a5b
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-09-11 21:33:03 +0200

    menu, links and scripts in head

--- public/assets/common/navbar.php
+++ public/assets/common/navbar.php
@@ -11,6 +11,11 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
         <?php
 
         foreach ($css_files as $css){
@@ -30,13 +35,13 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto links-margin">
                 <li class="nav-item link-margin">
-                    <a class="nav-link js-scroll-trigger link-hover" href="#design">Dodaj wydatek</a>
+                    <a class="nav-link js-scroll-trigger link-hover" href="#" data-toggle="modal" data-target="#addExpense">Dodaj wydatek</a>
                 </li>
                 <li class="nav-item link-margin">
-                    <a class="nav-link js-scroll-trigger link-hover" href="#features">Dodaj kategorie</a>
+                    <a class="nav-link js-scroll-trigger link-hover" href="#" data-toggle="modal" data-target="#addCategory">Dodaj kategorie</a>
                 </li>
                 <li class="nav-item link-margin">
-                    <a class="nav-link js-scroll-trigger link-hover" href="#responsive">Konto</a>
+                    <a class="nav-link js-scroll-trigger link-hover" href="#" data-toggle="modal" data-target="#account">Konto</a>
                 </li>
                 <li class="nav-item link-margin">
                     <a class="nav-link js-scroll-trigger link-hover" href="#clients">Wyloguj</a>
